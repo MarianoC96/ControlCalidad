644e729ddf93f3e3915b587ade259ca31f12bc5b
CMariano96 <m.castro.9605@gmail.com>
2026-03-12 13:50:52 -0500

    fix rutas

--- src/app/escaner-codigos/solicitudes/page.tsx
+++ src/app/escaner-codigos/solicitudes/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import RouteGuard from '@/components/RouteGuard';
-import SolicitudesClient from '@/app/solicitudes/SolicitudesClient';
+import SolicitudesClient from '@/app/control-sistema/centro-solicitudes/SolicitudesClient';
 
 export default function EscaneoSolicitudesPage() {
     return <RouteGuard moduleKey="solicitudes"><SolicitudesClient /></RouteGuard>;
